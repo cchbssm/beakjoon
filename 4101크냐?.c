357d02f758bb9cf9eb8d64561758a4120f45eb22
cchbssm <cheoc222@gmail.com>
2024-06-20 08:25:49 +0900

    Create 4101크냐?.c

---
+++ 4101크냐?.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2;
+    while(1){
+        scanf("%d%d", &n1, &n2);
+        if(n1==0 && n2==0) break;
+        printf("%s\n", (n1>n2)? "Yes" : "No");
+    }
+    
+    return 0;
+}
